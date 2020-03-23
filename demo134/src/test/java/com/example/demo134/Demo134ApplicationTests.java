ff0fb11d51d598d682b0704184ba0f847f854811
123 <2037236157@qq.com>
2020-03-23 19:27:02 +0800

    提交测试

---
+++ demo134/src/test/java/com/example/demo134/Demo134ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo134;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo134ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
